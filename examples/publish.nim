25ab425c06693df5bb35f48469f03026915e8706
onelivesleft <iainking@gmail.com>
2019-11-29 01:02:35 +0000

    Start of publish

--- examples/publish.nim
+++ examples/publish.nim
@@ -1,63 +1,90 @@
-import simple_parseopt, os, strutils, glob
-
-const TEMP_FILE = "include.tmp"
+import simple_parseopt, os, strutils
 
 
-simple_parseopt.config: can_name_bare
+help_text(
+    "Increase version and publish to github + nimble.",
+    "Will increase Patch version by default")
 
-var (options, supplied) = get_options_and_supplied:
-    make_docs     = false   {. alias("d"), info("Make docs file: `simple_parseopt.html`") .}
-    make_make     = false   {. alias("m"), info("Make this binary") .}
-    make_examples = false   {. alias("x"), info("Make other examples") .}
-    make_all      = false   {. alias("a") .}
-    open          = false   {. info("Open `simpleparseopt.html` after making it.") .}
-    include_file: string    {. alias("include"), info("File to include at top of doc"), bare .}
-    bin      = "bin/"       {. info("Folder to put compiled binaries") .}
-    src      = "src/"       {. info("Folder where `simple_parseopt.nim` resides") .}
-    examples = "examples/"  {. info("Folder where example .nim files reside") .}
-    version: string
+let options = get_options:
+    major = false
+    minor = false
+
+if options.major and options.minor:
+    quit("You cannot increase both major and minor versions")
 
 
-if options.make_all:
-    options.make_docs = true
-    options.make_make = true
-    options.make_examples = true
+if not os.exists_dir("src"):
+    os.set_current_dir("..")
+let simple_parseopt_path = os.join_path("src/simple_parseopt.nim")
+if not os.exists_file(path):
+    quit "Could not find simple_parseopt.nim"
 
 
-if options.make_docs:
-    if not supplied.include_file:
-        quit "You must specify an include_file"
-    if not os.exists_dir(options.src):
-        os.set_current_dir("..")
-    let path = os.join_path(options.src, "simple_parseopt.nim")
-    if not os.exists_file(path):
-        quit "Could not find simple_parseopt.nim"
-    if not os.exists_file(options.include_file):
-        quit "Could not find " & options.include_file
+var version = block:
+    let nimble = open("simple_parseopt.nimble")
+    defer: nimble.close()
 
-    if not os.exists_file("simple_parseopt.nimble"):
-        quit "Could not find `simple_parseopt.nimble`"
+    var found = ""
+    var line: string
+    while nimble.read_line(line):
+        if line.starts_with("version"):
+            found = line.split("\"")[1]
+            break
+    found
 
-    let version = block:
-        var result = ""
-        if supplied.version:
-            result = "v" & options.version
+if version == "":
+    quit "Could not find version"
+
+echo "\nCurrent version: v" & version
+
+var
+    parts = version.split(".")
+    major = parse_int(parts[0])
+    minor = parse_int(parts[1])
+    patch = parse_int(parts[2])
+
+if options.major:
+    major += 1
+    minor = 0
+    patch = 0
+elif options.minor:
+    minor += 1
+    patch = 0
+else:
+    patch += 1
+
+version = major.repr & "." & minor.repr & "." & patch.repr
+echo "New version:     v" & version
+
+
+proc alter_version(file_path, line_starts_with, replace_with: string) =
+    echo "\nUpdating `" & file_path.extract_filename & "` version..."
+
+    let tmp_path = file_path.change_file_ext(".tmp")
+
+    let in_file = open(file_path)
+    defer: in_file.close()
+
+    let out_file = open(tmp_path, fm_write)
+    defer: out_file.close()
+
+    var line: string
+    while in_file.read_line(line):
+        if line.starts_with():
+            out_file.write_line()
         else:
-            let nimble = open("simple_parseopt.nimble")
-            defer: nimble.close()
+            out_file.write_line(line)
 
-            var line: string
-            while nimble.read_line(line):
-                if line.starts_with("version"):
-                    result = "v" & line.split("\"")[1]
-                    break
-        result
+    os.remove_file(file_path)
+    os.move_file(tmp_path, nim_path)
 
-    if version == "":
-        quit "Could not find version"
+alter_version "src/simpl_parseopt.nim", "const VERSION", "const VERSION = \"" & version & "\""
+alter_version "simpl_parseopt.nimble",  "version ",      "version       = \"" & version & "\""
 
-    echo "\nPackage version: " & version
 
+
+
+#[
     block: #make temp file
         echo "\nGenerating header from " & options.include_file.extract_filename & "..."
         let in_file = open(options.include_file)
@@ -119,3 +146,4 @@ if options.make_examples:
 
             if error != 0:
                 quit "\nError compiling `" & file & "`: " & error.repr
+]#
